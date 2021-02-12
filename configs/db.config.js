203a6d5b994c99b057009bad0c11fedba8891618
Sebastian Montoya <seb246@gmail.com>
2021-02-11 20:09:48 -0500

    practicing with api , added api for cars and simple form to search by makes

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/change-the-db-name";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/car-api";
 
 mongoose
   .connect(MONGO_URI, {
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const { default: axios } = require('axios');
 
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
+
+router.get(`/cars`, (req, res, next) => {
+    res.render("cars");
+});
+
+router.get(`/car-info`, (req,res,next) => {
+
+        axios
+        .get(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${req.query.make}?format=json`)
+        .then(allCars => {
+            const cars = allCars.data;
+            console.log(cars);
+            res.render("cars-list", {cars});
+        })
+        .catch(err => {
+            console.log(`error getting car due to: ${err}`);
+        });
+
+
+
+});
+
 
 module.exports = router;
